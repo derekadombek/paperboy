75d2e31df60607b470ffa8e0759595b24c17a105
Derek Dombek <derekdombek@Dereks-MBP.hitronhub.home>
2020-10-27 09:38:59 -0600

    wip

---
+++ Assets/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    float min_speed = 5f;
+    float max_speed = 30f;
+    float rotation_speed = 100f;
+
+    Vector3 angles;
+
+    void Start()
+    {
+        angles = transform.eulerAngles;
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+        Camera.main.transform.position += transform.forward * speed * Time.deltaTime;
+        //hold
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            speed = max_speed;
+        }
+        //release
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            speed = min_speed;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            angles.y -= rotation_speed * Time.deltaTime;
+            angles.y = Mathf.Clamp(angles.y, -30, 30);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            angles.y += rotation_speed * Time.deltaTime;
+            angles.y = Mathf.Clamp(angles.y, -30, 30);
+        }
+
+        transform.rotation = Quaternion.Euler(angles);
+    }
+}
